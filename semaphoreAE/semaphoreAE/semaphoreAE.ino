dffae6c425f6636ef89ddc56900fe9b6caee1fb8
Andres Morilla <weamalisima@gmail.com>
2021-11-01 19:48:08 +0100

    Semaphore AE added

---
+++ semaphoreAE/semaphoreAE/semaphoreAE.ino
@@ -0,0 +1,427 @@
+#include "Wire.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+#define GREEN_PIN      D2
+#define YELLOW_PIN     D1
+#define RED_PIN        D0
+
+
+///////////////Parameters & Constants/////////////////
+// WIFI params
+char* WIFI_SSID = "sagemcom9800";    // Configure here the SSID of your WiFi Network
+char* WIFI_PSWD = "ATZNJMYCT2TYDZ"; // Configure here the PassWord of your WiFi Network
+int WIFI_DELAY  = 100; //ms
+const char* mqtt_server = "192.168.0.21";
+
+// oneM2M : CSE params
+String CSE_IP      = "192.168.0.21"; //Configure here the IP Address of your oneM2M CSE
+int   CSE_HTTP_PORT = 7579;
+String CSE_NAME    = "Mobius";
+String CSE_RELEASE = "3"; //Configure here the release supported by your oneM2M CSE
+bool ACP_REQUIRED = true; //Configure here whether or not ACP is required controlling access
+String ACPID = "";
+
+// oneM2M : resources' params
+String DESC_CNT_NAME = "DESCRIPTOR";
+String DATA_CNT_NAME = "DATA";
+String CMND_CNT_NAME = "COMMAND";
+String ACP_NAME = "MYACP";
+int TY_ACP  = 1;   
+int TY_AE  = 2;   
+int TY_CNT = 3;
+int TY_CI  = 4;
+int TY_SUB = 23;
+String originator = "CSemaphore";
+
+// HTTP constants
+int LOCAL_PORT = 80;
+char* HTTP_CREATED = "HTTP/1.1 201 CREATED";
+char* HTTP_OK    = "HTTP/1.1 200 OK\r\n";
+int REQUEST_TIME_OUT = 5000; //ms
+int REQUEST_NR = 0;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+StaticJsonDocument<1024> doc;
+
+//MISC
+#define DEBUG
+
+///////////////////////////////////////////
+
+// Global variables
+const long interval = 10000;
+long currentMillis;
+int SERIAL_SPEED  = 115200;
+int isYellow;
+
+WiFiClient client1;
+String context = "";        // The targeted actuator
+String command = "";        // The received command
+
+// Method for creating an HTTP POST with preconfigured oneM2M headers
+// param : url  --> the url path of the targted oneM2M resource on the remote CSE
+// param : ty --> content-type being sent over this POST request (2 for ae, 3 for cnt, etc.)
+// param : rep  --> the representaton of the resource in JSON format
+String doPOST(String url, String originator1, int ty, String rep) {
+
+  String relHeader = "";
+  if (CSE_RELEASE != "1") {
+    relHeader = "X-M2M-RVI: " + CSE_RELEASE + "\r\n";
+  }
+  String postRequest = String() + "POST " + url + " HTTP/1.1\r\n" +
+                       "Host: " + CSE_IP + ":" + CSE_HTTP_PORT + "\r\n" +
+                       "X-M2M-Origin: " + originator + "\r\n" +
+                       "Content-Type: application/json;ty=" + ty + "\r\n" +
+                       "Content-Length: " + rep.length() + "\r\n" +
+                       relHeader +
+                       "X-M2M-RI: req"+REQUEST_NR+"\r\n" +
+                       "Connection: close\r\n\r\n" +
+                       rep;
+
+  // Connect to the CSE address
+  Serial.println("connecting to " + CSE_IP + ":" + CSE_HTTP_PORT + " ...");
+
+  // Get a client
+  if (!client1.connect(CSE_IP, CSE_HTTP_PORT)) {
+    Serial.println("Connection failed !");
+    return "error";
+  }
+
+  // if connection succeeds, we show the request to be send
+#ifdef DEBUG
+  Serial.println(postRequest);
+#endif
+
+  // Send the HTTP POST request
+  client1.print(postRequest);
+
+  //Update request number after each sending
+  REQUEST_NR += 1;
+  
+  // Manage a timeout
+  unsigned long startTime = millis();
+  while (client1.available() == 0) {
+    if (millis() - startTime > REQUEST_TIME_OUT) {
+      Serial.println("Client Timeout");
+      client1.stop();
+      return "error";
+    }
+  }
+
+  // If success, Read the HTTP response
+  String result = "";
+  client1.setTimeout(500);
+  if (client1.available()) {
+    result = client1.readStringUntil('\r');
+    Serial.println(result);
+  }
+  while (client1.available()) {
+    String line = client1.readStringUntil('\r');
+    Serial.print(line);
+  }
+  Serial.println();
+  Serial.println("closing connection...");
+  return result;
+}
+
+// Method for creating an ApplicationEntity(AE) resource on the remote CSE (this is done by sending a POST request)
+// param : ae --> the AE name (should be unique under the remote CSE)
+String createAE(String ae) {
+  String srv = "";
+    if(CSE_RELEASE != "1"){
+      srv = ",\"srv\":[\""+CSE_RELEASE+"\"]";
+    }
+  String aeRepresentation =
+    "{\"m2m:ae\": {"
+    "\"rn\":\"" + ae + "\","+
+    "\"api\":\"N.org.demo." + ae + "\","
+    "\"rr\":true,"
+    "\"poa\":[\"http://" + WiFi.localIP().toString() + ":" + LOCAL_PORT + "/" + ae + "\"]" +
+    srv +
+    "}}";
+#ifdef DEBUG
+  Serial.println(aeRepresentation);
+#endif
+  return doPOST("/" + CSE_NAME, originator, TY_AE, aeRepresentation);
+}
+
+// Method for creating an Access Control Policy(ACP) resource on the remote CSE under a specific AE (this is done by sending a POST request)
+// param : ae --> the targeted AE name (should be unique under the remote CSE)
+// param : acp  --> the ACP name to be created under this AE (should be unique under this AE)
+String createACP(String ae, String acp) {
+  String acpRepresentation =
+    "{\"m2m:acp\": {"
+  "\"rn\":\"" + acp + "\","
+  "\"pv\":{\"acr\":[{\"acor\":[\"all\"],\"acop\":63}]},"
+  "\"pvs\":{\"acr\":[{\"acor\":[\"all\"],\"acop\":63}]}"
+  "}}";
+  return doPOST("/" + CSE_NAME + "/" + ae, originator, TY_ACP, acpRepresentation);
+}
+
+// Method for creating an Container(CNT) resource on the remote CSE under a specific AE (this is done by sending a POST request)
+// param : ae --> the targeted AE name (should be unique under the remote CSE)
+// param : cnt  --> the CNT name to be created under this AE (should be unique under this AE)
+String createCNT(String ae, String cnt) {
+  String cntRepresentation =
+    "{\"m2m:cnt\": {"
+    "\"mni\":10,"         // maximum number of instances
+    "\"rn\":\"" + cnt + "\"" +
+    ACPID + //IF ACP created, it is associated to the container so that anyone has access 
+    "}}";
+  return doPOST("/" + CSE_NAME + "/" + ae, originator, TY_CNT, cntRepresentation);
+}
+
+// Method for creating an ContentInstance(CI) resource on the remote CSE under a specific CNT (this is done by sending a POST request)
+// param : ae --> the targted AE name (should be unique under the remote CSE)
+// param : cnt  --> the targeted CNT name (should be unique under this AE)
+// param : ciContent --> the CI content (not the name, we don't give a name for ContentInstances)
+String createCI(String ae, String cnt, String ciContent) {
+  String ciRepresentation =
+    "{\"m2m:cin\": {"
+    "\"con\":\"" + ciContent + "\""
+    "}}";
+  return doPOST("/" + CSE_NAME + "/" + ae + "/" + cnt, originator,  TY_CI, ciRepresentation);
+}
+
+
+// Method for creating an Subscription (SUB) resource on the remote CSE (this is done by sending a POST request)
+// param : ae --> The AE name under which the SUB will be created .(should be unique under the remote CSE)
+//          The SUB resource will be created under the COMMAND container more precisely.
+String createSUB(String ae) {
+  String subRepresentation =
+    "{\"m2m:sub\": {"
+    "\"rn\":\"SUB_" + ae + "\","
+    "\"nu\":[\"mqtt://" + "localhost" + "/" +  originator  + "?ct=json\"], "
+    "\"enc\":{\"net\":[3]}"
+    "}}";
+  return doPOST("/" + CSE_NAME + "/" + ae + "/" + CMND_CNT_NAME, originator,  TY_SUB, subRepresentation);
+}
+
+String subscription(String subscriptor, String broadcaster) {
+  String subRepresentation =
+    "{\"m2m:sub\": {"
+    "\"rn\":\"SUB_" + subscriptor + "\","
+    "\"nu\":[\"mqtt://" + "localhost" + "/" +  originator  + "?ct=json\"], "
+    "\"enc\":{\"net\":[3]}"
+    "}}";
+  return doPOST("/" + CSE_NAME + "/" + broadcaster + "/" + DATA_CNT_NAME, originator,  TY_SUB, subRepresentation);
+}
+
+
+// Method to register a module (i.e. sensor or actuator) on a remote oneM2M CSE
+void registerModule(String module, bool isActuator, String intialDescription, String initialData) {
+  if (WiFi.status() == WL_CONNECTED) {
+    String result;
+    
+    // 1. Create the ApplicationEntity (AE) for this sensor
+    result = createAE(module);
+    if (result.equalsIgnoreCase(HTTP_CREATED)) {
+      #ifdef DEBUG
+      Serial.println("AE " + module + " created  !");
+      #endif
+      // 1.1 Create the AccessControlPolicy (ACP) for this sensor so that monitor can subscribe to it
+      if(ACP_REQUIRED) {
+        result = createACP(module, ACP_NAME);
+        if (result.equalsIgnoreCase(HTTP_CREATED)) {
+          ACPID = ",\"acpi\":[\"" + CSE_NAME + "/" + module + "/" + ACP_NAME + "\"]";
+          #ifdef DEBUG
+            Serial.println("ACP " + module + " created  !");
+          #endif
+        }
+      }
+      // 2. Create a first container (CNT) to store the description(s) of the sensor
+      result = createCNT(module, DESC_CNT_NAME);
+      if (result.equalsIgnoreCase(HTTP_CREATED)) {
+        #ifdef DEBUG
+        Serial.println("CNT " + module + "/" + DESC_CNT_NAME + " created  !");
+        #endif
+        // Create a first description under this container in the form of a ContentInstance (CI)
+        result = createCI(module, DESC_CNT_NAME, intialDescription);
+        if (result.equalsIgnoreCase(HTTP_CREATED)) {
+          #ifdef DEBUG
+          Serial.println("CI " + module + "/" + DESC_CNT_NAME + "/{initial_description} created !");
+          #endif
+        }
+      }
+      // 3. Create a second container (CNT) to store the data  of the sensor
+      result = createCNT(module, DATA_CNT_NAME);
+      if (result.equalsIgnoreCase(HTTP_CREATED)) {
+        #ifdef DEBUG
+        Serial.println("CNT " + module + "/" + DATA_CNT_NAME + " created !");
+        #endif
+        // Create a first data value under this container in the form of a ContentInstance (CI)
+        result = createCI(module, DATA_CNT_NAME, initialData);
+        if (result.equalsIgnoreCase(HTTP_CREATED)) {
+          #ifdef DEBUG
+          Serial.println("CI " + module + "/" + DATA_CNT_NAME + "/{initial_data} created !");
+          #endif
+        }
+      }
+
+      // 4. if the module is an actuator, create a third container (CNT) to store the received commands
+      if (isActuator) {
+        result = createCNT(module, CMND_CNT_NAME);
+        if (result.equalsIgnoreCase(HTTP_CREATED)) {
+          #ifdef DEBUG
+          Serial.println("CNT " + module + "/" + CMND_CNT_NAME + " created !");
+          #endif
+          // subscribe to any command put in this container
+          result = createSUB(module);
+          if (result.equalsIgnoreCase(HTTP_CREATED)) {
+            #ifdef DEBUG
+            Serial.println("SUB " + module + "/" + CMND_CNT_NAME + "/SUB_" + module + " created !");
+            #endif
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
+void init_WiFi() {
+  Serial.println("Connecting to  " + String(WIFI_SSID) + " ...");
+  WiFi.persistent(false);
+  WiFi.begin(WIFI_SSID, WIFI_PSWD);
+
+  // wait until the device is connected to the wifi network
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(WIFI_DELAY);
+    Serial.print(".");
+  }
+
+  // Connected, show the obtained ip address
+  Serial.println("WiFi Connected ==> IP Address = " + WiFi.localIP().toString());
+}
+void task_WiFi() {
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      //client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      //client.subscribe("#");
+      String s = "/oneM2M/+/" + originator + "/+/#";
+      char topic[50]; 
+      s.toCharArray(topic, 50);
+      client.subscribe(topic); 
+
+      s = "/oneM2M/resp/" + originator + "/+/#";
+      char topic2[50];
+      s.toCharArray(topic2, 50);
+      client.subscribe(topic2);
+
+      s = "/oneM2M/req/+/" + originator + "/#";
+      char topic3[50];
+      s.toCharArray(topic3, 50);
+      client.subscribe(topic3);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+String process(String json) {
+  DeserializationError error = deserializeJson(doc, json);
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return "";
+    }
+    return doc["pc"]["m2m:sgn"]["nev"]["rep"]["m2m:cin"]["con"];
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+
+  String strPayload = String((char*)payload);
+  String data = process(strPayload);
+  Serial.println();
+  Serial.print("Recived: " + data);
+  Serial.println();
+
+  if(data != NULL){                                            //CAMBIAR POR CADA AE NUEVO
+    if(data == "red") {                                                             //CAMBIAR POR CADA AE NUEVO
+      isYellow = 0;
+      digitalWrite(GREEN_PIN,LOW);
+      digitalWrite(YELLOW_PIN,LOW); 
+      digitalWrite(RED_PIN,HIGH);                                                   //CAMBIAR POR CADA AE NUEVO
+    } else if(data == "green") {                             //CAMBIAR POR CADA AE NUEVO
+      isYellow = 0;
+      digitalWrite(YELLOW_PIN,LOW);
+      digitalWrite(GREEN_PIN,HIGH);                        //CAMBIAR POR CADA AE NUEVO
+      digitalWrite(RED_PIN,LOW);
+    }else if(data=="yellow"){
+      isYellow = 1;
+      digitalWrite(RED_PIN,LOW);
+      digitalWrite(GREEN_PIN,LOW);
+    }
+  }
+
+}
+
+void init_Semaphore(){
+  String initialDescription = "Name=Semaphore;Location=Parking";
+  String initialData = "Red";
+  originator = "CSemaphore";
+  registerModule("Semaphore", true, initialDescription, initialData);
+}
+
+
+void setup() {
+  // intialize the serial liaison
+  Serial.begin(SERIAL_SPEED);
+  //Set up mqtt client
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  client.setBufferSize(1024);
+
+  // Connect to WiFi network
+  init_WiFi();
+
+  // register sensors and actuators
+  init_Semaphore();
+  //set SUBs
+  //originator = "CSemaphore";
+
+}
+
+// Main loop of the µController
+void loop() {
+  
+  if(isYellow==1) {
+    digitalWrite(YELLOW_PIN,HIGH);
+    delay(500);
+    digitalWrite(YELLOW_PIN,LOW);
+  }
+  
+   if (!client.connected()) {
+    reconnect();
+  } client.loop();
+
+
+  
+}
