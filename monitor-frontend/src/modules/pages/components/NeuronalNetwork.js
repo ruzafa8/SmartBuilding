bf4ba0557e2d2355a7334ab061443c5854c9c78f
ruzafa8 <ruzafa8@gmail.com>
2021-11-03 22:55:43 +0100

    Connected frontend's Neuronal Network's chart to database

--- monitor-frontend/src/modules/pages/components/NeuronalNetwork.js
+++ monitor-frontend/src/modules/pages/components/NeuronalNetwork.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Donut } from "../../chart";
-
+import database from "../../../database";
 const Flex = styled.div`
     display:flex;
     flex-wrap:wrap;
@@ -14,13 +14,23 @@ const Box = styled.div`
     width:40%;
 `
 
-const NeuronalNetwork = () => <Flex>
-    <Box>
-        <Donut labels={['Car detections', 'False detections']} data={[230,20]}/>
-    </Box>
-    <Box>
-        <Donut labels={['Cars that belongs', 'Cars that don\'t belong']} data={[180,20]}/>
-    </Box>
-</Flex>
-
+const NeuronalNetwork = () => {
+    const [detections, setDetections] = useState({belongs:[0,0], trueDetection:[0,0]});
+    useEffect(() => {
+        database.getDetectionsType(res => {
+            setDetections({belongs: [res.BELONG, res.NOT_BELONG],
+                 trueDetection: [res.TRUE_DETECTION, res.FALSE_DETECTION]});
+        }, () => {
+            setDetections({belongs:[230,20], trueDetection:[180,20]});
+        });
+    },[])
+    return <Flex>
+        <Box>
+            <Donut labels={['Car detections', 'False detections']} data={detections.belongs}/>
+        </Box>
+        <Box>
+            <Donut labels={['Cars that belongs', 'Cars that don\'t belong']} data={detections.trueDetection}/>
+        </Box>
+    </Flex>
+}
 export default NeuronalNetwork;
